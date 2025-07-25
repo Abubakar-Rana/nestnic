eed98e40d8ff62a0f9fd2e7a1572f3421a1b99f7
Abubakar-Rana <f2021-641@bnu.edu.pk>
2025-07-26 03:54:46 +0500

    Initial commit: Nestnic Solutions website with dark theme and hydration fix

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ['placehold.co', 'static.tossdown.com','cdn.prod.website-files.com'], // Added static.tossdown.com to allowed domains
+  },
 };
 
 export default nextConfig;
